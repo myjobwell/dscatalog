baf1661e694ad374666cb767e9fb654e4ddc06d2
myjobwell <70344092+myjobwell@users.noreply.github.com>
2025-02-25 17:11:12 -0500

    adicionando paginação nas consultas

--- backend/src/main/java/com/devwell/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devwell/dscatalog/resources/CategoryResource.java
@@ -2,16 +2,17 @@ package com.devwell.dscatalog.resources;
 
 
 import com.devwell.dscatalog.dto.CategoryDTO;
-import com.devwell.dscatalog.entities.Category;
 import com.devwell.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 @RestController
@@ -23,8 +24,14 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = categoryService.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/java/com/devwell/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devwell/dscatalog/services/CategoryService.java
@@ -8,13 +8,14 @@ import com.devwell.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Service
 public class CategoryService {
@@ -25,9 +26,9 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
 //    tag acima inicia uma transação e nao trava o banco de dados durante a consulta
-    public List<CategoryDTO> findAll() {
-        List<Category> list = categoryRepository.findAll();
-        return list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> list = categoryRepository.findAll(pageRequest);
+        return list.map(CategoryDTO::new);
     }
 
     @Transactional(readOnly = true)
