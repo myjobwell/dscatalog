ecf36b69db3c2790694a98e2e62b96e7592164f0
myjobwell <70344092+myjobwell@users.noreply.github.com>
2025-02-28 16:01:18 -0500

    Validação customizada User

--- backend/src/main/java/com/devwell/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devwell/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.devwell.dscatalog.dto;
 
+import com.devwell.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private static final long serialVersionUID = 1L;
 
